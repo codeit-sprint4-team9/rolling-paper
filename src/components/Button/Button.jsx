988fefc5cb8998f4915f27e8352786cc7c7c2d7f
JA_X <envijs93@gmail.com>
2024-02-29 11:56:59 +0900

    Feat: size 프롭을 지우고 width 속성을 줘서 길이 커스텀가능하게 완료

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,13 +1,19 @@
 import css from './Button.module.scss';
-const Button = ({ children, type, size, isDisabled = false, onclick }) => {
+const Button = ({ children, type, width, isDisabled = false, onClick }) => {
   let buttonClassName = `${css.primary}`;
+  if (type === 'primary-s') buttonClassName += ` ${css.small}`;
   if (type === 'secondary') buttonClassName = `${css.secondary} ${css.small}`;
-  if (size === 'small') buttonClassName += ` ${css.small}`;
-  if (size === 'full') buttonClassName += ` ${css.full}`;
 
   return (
     <>
-      <button className={buttonClassName} disabled={isDisabled} onClick={onclick}>
+      <button
+        className={buttonClassName}
+        disabled={isDisabled}
+        onClick={onClick}
+        style={{
+          width: `${width}`,
+        }}
+      >
         {children}
       </button>
     </>
